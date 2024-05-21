645ad95d45a9accff1b5ebe626ee80a745ea24c4
asadabbasov2004 <>
2024-05-21 15:23:19 +0400

    do complete

---
+++ Petshop/Models/TeamMember.cs
@@ -0,0 +1,10 @@
+﻿namespace Petshop.Models
+{
+    public class TeamMember
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; } = null!;
+        public string Profession { get; set; } = null!;
+    }
+}
